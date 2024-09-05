aeae2f768df2445ce4bc3d2ce5e80ea38ccdd26c
Duan2baka <duan2baka@gmail.com>
2024-09-05 16:32:26 +0800

    Modify the command list

--- commands.js
+++ commands.js
@@ -33,6 +33,60 @@ const LOVE_COMMAND = {
   contexts: [0, 1, 2],
 };
 
+const SHOWLIST_COMMAND = {
+  name: 'show',
+  description: 'Show all lists created',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+const CREATELIST_COMMAND = {
+  name: 'create',
+  description: 'Show all lists created',
+  options: [
+    {
+      type: 3,
+      name: 'name',
+      description: 'Name your list',
+      required: true
+    },
+  ],
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+const ADDTOLIST_COMMAND = {
+  name: 'add_to_list',
+  description: 'Add an entry to your list',
+  options: [
+    {
+      type: 3,
+      name: 'entry',
+      description: 'Entry name',
+      required: true
+    },
+    {
+      type: 3,
+      name: 'list',
+      description: 'List name',
+      required: true
+    },
+  ],
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+const REMOVELIST_COMMAND = {
+  name: 'remove',
+  description: 'Remove a list',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
 // Command containing options
 const CHALLENGE_COMMAND = {
   name: 'challenge',
@@ -51,6 +105,6 @@ const CHALLENGE_COMMAND = {
   contexts: [0, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, LOVE_COMMAND, CHALLENGE_COMMAND];
+const ALL_COMMANDS = [TEST_COMMAND, LOVE_COMMAND, CREATELIST_COMMAND, REMOVELIST_COMMAND, ADDTOLIST_COMMAND, SHOWLIST_COMMAND, CHALLENGE_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
